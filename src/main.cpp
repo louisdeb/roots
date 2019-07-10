38e8641995416d48020c521964ed709bea96515b
Louis de Beaumont <debeaumont.louis@gmail.com>
2019-07-10 19:16:21 +0100

    Create AP and accept Wifi credentials and canopy user auth token

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,26 @@
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
 
+const char *AP_NAME = "roots";
+const char *AP_PASS = "password";
+
 void setup() {
   Serial.begin(9600);
 
+  WiFi.disconnect(); // Removes saved WiFi credentials
+
+  const char* authToken = (char*)malloc(24 * sizeof(char));
+  int tokenLength = 24;
+  WiFiManagerParameter authParam("auth", "canopy user auth", authToken, tokenLength);
+
   WiFiManager wifiManager;
-  wifiManager.autoConnect("roots", "password");
+  wifiManager.addParameter(&authParam);
+  wifiManager.autoConnect(AP_NAME, AP_PASS);
+
+  authToken = authParam.getValue();
+
+  Serial.println("connected");
+  Serial.println(authToken);
 }
 
 void loop() {
